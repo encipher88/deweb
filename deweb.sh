6fb703d5f015f0adf29ae2fa7244f8dfb3b2bbff
encipher <36136421+encipher88@users.noreply.github.com>
2022-05-03 16:59:52 +0300

    Update deweb.sh

--- deweb.sh
+++ deweb.sh
@@ -1,13 +1,5 @@
 #!/bin/bash
-exists()
-{
-  command -v "$1" >/dev/null 2>&1
-}
-if exists curl; then
-echo ''
-else
-  sudo apt update && sudo apt install curl -y < "/dev/null"
-fi
+sudo apt update && sudo apt install curl -y 
 bash_profile=$HOME/.bash_profile
 if [ -f "$bash_profile" ]; then
     . $HOME/.bash_profile
